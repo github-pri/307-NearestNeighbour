38ad511e087e06a1a7df9d19df112838df939856
Pri Bhula <pridotbhula@gmail.com>
2017-03-23 22:21:09 +1300

    Addded calc methods

--- src/Iris.java
+++ src/Iris.java
@@ -3,16 +3,31 @@
  */
 public class Iris {
 
-    private double sepalLength;
-    private double sepalWidth;
-    private double petalLength;
-    private double petalWidth;
+    private double sepalLength, sepalWidth, petalLength, petalWidth;
+    private String type;
 
-    public Iris (double sepalLength, double sepalWidth, double petalLength, double petalWidth){
+    public Iris (double sepalLength, double sepalWidth, double petalLength, double petalWidth,String type){
         this.sepalLength = sepalLength;
         this.sepalWidth = sepalWidth;
         this.petalLength = petalLength;
         this.petalWidth = petalWidth;
+        this.type = type;
     }
+
+    public double getSepalLength(){
+        return sepalLength;
+    }
+
+    public double getSepalWidth() {
+        return sepalWidth;
+    }
+
+    public double getPetalLength() {
+        return petalLength;
+    }
+
+    public double getPetalWidth() { return petalWidth; }
+
+    public String getType(){ return type; }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,7 @@ public class Main {
                 double petalWidth = scanTrain.nextDouble();
                 String type = scanTrain.next();
 
-                trainingSet.add(new Iris(sepalLength,sepalWidth,petalLength,petalWidth));
+                trainingSet.add(new Iris(sepalLength,sepalWidth,petalLength,petalWidth, type));
             }
 
             while (scanTest.hasNext()){
@@ -33,7 +33,7 @@ public class Main {
                 double petalWidth = scanTrain.nextDouble();
                 String type = scanTrain.next();
 
-                testingSet.add(new Iris(sepalLength,sepalWidth,petalLength,petalWidth));
+                testingSet.add(new Iris(sepalLength,sepalWidth,petalLength,petalWidth,type));
             }
             scanTrain.close();
             scanTest.close();
@@ -42,7 +42,83 @@ public class Main {
             e.printStackTrace();
         }
 
+    }
 
+    private double calculateRange(ArrayList<Iris> instances, String type){
+        double min=0;
+        double max=0;
+
+        if (instances.isEmpty()){
+            return 0;
+        }
+
+        if (type=="sepalLength"){
+            max = instances.get(0).getSepalLength();
+            min = max;
+            for (Iris i:instances){
+                if (i.getSepalLength()>max){
+                    max = i.getSepalLength();
+                }
+                else if (i.getSepalLength()<min){
+                    min = i.getSepalLength();
+                }
+            }
+        }
+
+        else if (type=="sepalWidth"){
+            max = instances.get(0).getSepalWidth();
+            min = max;
+            for (Iris i:instances){
+                if (i.getSepalWidth()>max){
+                    max = i.getSepalWidth();
+                }
+                else if (i.getSepalWidth()<min){
+                    min = i.getSepalWidth();
+                }
+            }
+        }
+
+        else if (type=="petalLength"){
+            max = instances.get(0).getPetalLength();
+            min = max;
+            for (Iris i:instances){
+                if (i.getPetalLength()>max){
+                    max = i.getPetalLength();
+                }
+                else if (i.getPetalLength()<min){
+                    min = i.getPetalLength();
+                }
+            }
+        }
+
+        else if (type=="petalWidth"){
+            max = instances.get(0).getPetalWidth();
+            min = max;
+            for (Iris i:instances){
+                if (i.getPetalWidth()>max){
+                    max = i.getPetalWidth();
+                }
+                else if (i.getPetalWidth()<min){
+                    min = i.getPetalWidth();
+                }
+            }
+        }
+
+        return (max - min);
+    }
+
+    public double calculateDistance(Iris i1, Iris i2){
+        double sLRange = calculateRange(trainingSet,"sepalLength");
+        double swRange = calculateRange(trainingSet, "sepalWidth");
+        double pLRange = calculateRange(trainingSet, "petalLength");
+        double pwRange = calculateRange(trainingSet, "petalWidth");
+
+        double petalL = Math.pow((i1.getPetalLength() - i2.getPetalLength())/pLRange,2);
+        double petalW = Math.pow((i1.getPetalWidth() - i2.getPetalWidth())/pwRange,2);
+        double sepalL = Math.pow((i1.getSepalLength() - i2.getSepalLength())/sLRange,2);
+        double sepalW = Math.pow((i1.getSepalWidth() - i2.getSepalWidth())/swRange, 2);
+
+        return Math.sqrt(petalL + petalW + sepalL + sepalW);
     }
 
 }
